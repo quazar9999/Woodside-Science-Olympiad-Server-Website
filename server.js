c0ca38b128a0fc967368ddbc1370729c52e4147c
quazar9999 <ystep.gd.351@gmail.com>
2026-02-27 20:17:52 -0800

    Set up port listening

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const http = require ('http');
+const port = 3000; //change
+const server = http.createServer(function(req,res){
+    res.write("hello world");
+    res.end()
+})
+
+server.listen(port,function(error){
+    if(error){
+        console.log("there was an error: ", error);
+    }else{
+        console.log("listening on port: ", port);
+    }
+})
