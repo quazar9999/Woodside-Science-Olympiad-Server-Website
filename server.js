2dccff0c49267223ec81df2aa129b984c7b52764
quazar9999 <ystep.gd.351@gmail.com>
2026-02-27 21:22:10 -0800

    Fetch specific file and load 'home page'

--- server.js
+++ server.js
@@ -3,17 +3,36 @@ const fs = require('fs')
 const port = 3000;
 
 const server = http.createServer(function(req,res){
+    method = req.method;
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('dashboard.html', function(error,data){
-        if(error){
-            res.writeHead(404);
-            res.write("Error: File not found");
+    if (method === 'GET'){
+        //If the url is blank, redirect to the dashboard
+        if(req.url='/.well-known/appspecific/com.chrome.devtools.json'){
+            fs.readFile("./site/dashboard.html",function(error,data){
+                if(error){
+                    res.writeHead(404);
+                    res.write("Error: File not found");
+                }else{
+                    res.write(data);
+                }
+                res.end();
+            })
         }else{
-            res.write(data);
-        } 
-        res.end();
-    })
+            //Otherwise, load the requested file
+            fs.readFile("."+req.url, function(error,data){
+                if(error){
+                    res.writeHead(404);
+                    res.write("Error: File not found");
+                }else{
+                    res.write(data);
+                } 
+                res.end();
+            })
+        }
+    }
+
+    
+    
 })
 
 server.listen(port,function(error){
